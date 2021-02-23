f7fe2251ed977e3342fe80b880c003c7ab0dae60
CHALET <emerick.chalet@edu.itescia.fr>
2021-02-23 21:39:49 +0100

    start story 1

---
+++ app/src/main/java/com/g2/musique/QuestionActivity.java
@@ -0,0 +1,41 @@
+package com.g2.musique;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class QuestionActivity extends AppCompatActivity {
+
+    private MediaPlayer mediaPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question);
+
+        this.mediaPlayer = MediaPlayer.create(getApplicationContext(),R.raw.the_week_end);
+    }
+
+    public void playSound(View view) {
+
+        Button button = (Button) view;
+
+        if (mediaPlayer.isPlaying())
+        {
+            mediaPlayer.pause();
+            button.setText(getString(R.string.play_music_btn));
+        }
+        else if(!mediaPlayer.isLooping())
+        {
+            button.setText(getString(R.string.play_music_btn));
+        }
+        else
+        {
+            mediaPlayer.start();
+            button.setText(getString(R.string.pause_music_btn));
+        }
+    }
+}
