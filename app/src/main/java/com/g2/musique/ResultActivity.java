fd62aa97402638e879a0c4ff9291d134586e894e
Lexo783 <florentdebuchy@orange.fr>
2021-02-24 20:50:19 +0100

    add percentage

--- app/src/main/java/com/g2/musique/ResultActivity.java
+++ app/src/main/java/com/g2/musique/ResultActivity.java
@@ -4,13 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class ResultActivity extends AppCompatActivity {
 
     public static final String EXTRA_SCORE_QUESTION = "numberScore";
+    private ArrayList<Question> questionsList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +22,17 @@ public class ResultActivity extends AppCompatActivity {
         setContentView(R.layout.activity_result);
 
         Intent srcIntent = getIntent();
-
-
-        TextView textScore = findViewById(R.id.realizedTextView3);
+        questionsList = srcIntent.getParcelableArrayListExtra("questions");
         int score = srcIntent.getIntExtra(EXTRA_SCORE_QUESTION,0);
 
-        textScore.setText(score + " !");
+        float percentage = (score * 100) / questionsList.size();
+
+        Log.i("test", "onCreate: "+ percentage);
+
+        TextView textScore = findViewById(R.id.realizedTextView3);
+
+
+        textScore.setText(percentage + "%");
 
         Button homeButton = findViewById(R.id.homeButton);
 
